d33988863a06de38f9ee91f3a211736365b42255
Nilesh verma <nv1082003@gmail.com>
2024-03-20 23:52:28 +0530

    fixed toggle

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,15 @@ export default function Home() {
     
   ];
 
+  const [isClosed, setIsClosed] = useState(false);
+
+    const toggleClose = () => {
+        setIsClosed(!isClosed);
+    };
+
   return (
     <div className="flex h-screen">
-      <div className="bg-[#111827] w-1/3 flex flex-col">
+      <div className={`bg-[#111827] w-1/3 flex flex-col ${isClosed ? 'hidden' : ''}`}>
         <div className="flex-grow">
           <UserLogo />
           {onlinePeopleExclOurUser.map((user) => (
@@ -35,7 +41,21 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="bg-white w-2/3 flex flex-col">
+      <div className="top-auto flex justify-center items-center h-screen">
+      {isClosed ? (
+                
+      <svg onClick={toggleClose} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+       <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+      </svg>
+
+              ) : (
+
+                <svg onClick={toggleClose} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+              </svg>
+            )}
+            </div>
+      <div className={`flex flex-col bg-white ${isClosed ? 'w-full' : 'w-2/3'} p-2`}>
   {!selectedUserId && <div>wrong select</div>}
   {!!selectedUserId && (
     <div>
@@ -50,7 +70,6 @@ export default function Home() {
     </div>
   )}
 </div>
-
 
     </div>
   );
